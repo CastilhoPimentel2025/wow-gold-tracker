4328017227b58542e2ad3d301fc00f0c6647d1ff
Leonardo Castilho <leonardocastilho858@gmail.com>
2026-04-01 23:36:37 -0300

    0

--- scripts/package.sh
+++ scripts/package.sh
@@ -5,4 +5,12 @@ VERSION=$(grep "## Version" addon/GoldTracker.toc | awk '{print $3}')
 
 mkdir -p dist
 
-zip -r "dist/${ADDON_NAME}-${VERSION}.zip" addon/
+# Copia o conteúdo de addon/ para uma pasta com o nome correto
+cp -r addon/ "dist/${ADDON_NAME}"
+
+# Empacota com a estrutura correta
+cd dist
+zip -r "${ADDON_NAME}-${VERSION}.zip" "${ADDON_NAME}/"
+
+# Limpa a pasta temporária
+rm -rf "${ADDON_NAME}"
